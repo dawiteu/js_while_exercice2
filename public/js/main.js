f2e5ac262f8d2d10de7760b8293040974f09d81a
DawiT <Dawid@Tararuj.be>
2021-01-11 16:33:57 +0100

    exo 3

--- public/js/main.js
+++ public/js/main.js
@@ -41,7 +41,7 @@ while(nbr <= 11){
 
 */
 
-
+/*
 // exo 2 (2)
 
 let prenom = prompt("Entre ton prenom: "); 
@@ -58,15 +58,67 @@ while(max > 0){
 // soit ca 
 
 let min = 0; 
-/*
+
 while(min < prenom.length){ 
     console.log(`${prenom}`);
     min++; 
 }
 
-*/
+
 
 while(min < prenom.length){
     console.log(prenom.charAt(min));
     min++;
 }
+
+
+*/
+
+//exo 3 
+
+
+/*
+let prenom = prompt("Entre ton prenom");
+
+let min = 0; 
+
+while(min < prenom.length){
+    if(min % 2 == 0){
+        console.log(` ${ prenom.charAt(min) }`);
+    }else{
+        console.log(` ${ prenom.charAt(min).toUpperCase() }`);
+    }
+    min++;
+}
+
+*/
+
+// exo 3 (2) 
+
+
+let nb1 = prompt("Entre le nombre 1"); 
+let nb2 = prompt("Entrez le nombre 2 "); 
+
+while(isNaN(nb1)){
+    nb1 = parseFloat(prompt("Entre le nombre 1")); 
+}
+while(isNaN(nb2)){
+    nb2 = parseFloat(prompt("Entre le nombre 2")); 
+}
+
+console.log(`${nb1} x ${nb2} = ${Math.round(nb1*nb2)} `);
+
+
+/* /*
+
+let prenom = prompt("Entre ton prenom?"); 
+
+while(prenom.length < 5){
+    prenom = prompt("Entre ton prenom!?");
+}
+
+if(prenom.length >= 5){
+    console.log(prenom.toUpperCase()); 
+}
+
+*/
