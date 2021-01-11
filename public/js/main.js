f3608ee67b1ea82ac419eb46aca9c107c1f715a9
DawiT <Dawid@Tararuj.be>
2021-01-11 15:20:21 +0100

    exo 2

--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,50 @@ if(!isNaN(nombre)){
 }
 
 */
+
+/* 
+
+//exo 2 (1): 
+
+
+
+let nbr = 0; 
+
+while(nbr <= 11){
+    if(nbr%2==1){
+       console.log(nbr); 
+    }
+    nbr++; 
+}
+
+*/
+
+
+// exo 2 (2)
+
+let prenom = prompt("Entre ton prenom: "); 
+
+console.log(`Nombre de char dans ${prenom} = ${prenom.length}`); 
+
+let max = prenom.length; 
+
+while(max > 0){
+    console.log(`${prenom} `); 
+    max--;
+}
+
+// soit ca 
+
+let min = 0; 
+/*
+while(min < prenom.length){ 
+    console.log(`${prenom}`);
+    min++; 
+}
+
+*/
+
+while(min < prenom.length){
+    console.log(prenom.charAt(min));
+    min++;
+}
